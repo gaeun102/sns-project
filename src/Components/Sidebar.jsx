39b18d91df8f35a7c308267ea2bc9d54c286db00
gaeunSung <ske40109@gmail.com>
2023-12-28 00:28:20 +0900

    Sidebar

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { IoReorderThreeOutline } from "react-icons/io5";
+import { mainu } from './SidebarConfig';
+
+const Sidebar = () => {
+  return (
+    <div className='sticky top-0 h-[100vh]'>
+      <div className='flex flex-col justify-between h-full'>
+        <div>
+            <div className='pt-10'>
+            <img className='w-40' src="https://i.imgur.com/zqpwkLQ.png" alt="" /> {/* 인스타그램 로고*/}
+          </div>
+        <div className='mt-10'>
+          {mainu.map((item) => (
+            <div className='flex items-center mb-5 cursor-pointer text-lg'>
+              {item.icon}
+              <p>{item.title}</p>
+            </div>
+          ))}
+        </div>
+        </div>
+        <div className='flex items-center cursor-pointer pb-10'>
+          <IoReorderThreeOutline />
+          <p className='ml-5'>More</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/Components/SidebarConfig.js
@@ -0,0 +1,39 @@
+import { AiOutlineCompass,AiOutlineSearch,AiFillCompass, AiFillHome, AiFillMessage, AiFillPlusCircle, AiOutlineHeart, AiOutlineHome, AiOutlineMessage, AiOutlinePlusCircle } from "react-icons/ai"
+import {RiVideoFill,RiVideoLine} from "react-icons/ri"
+import {CgProfile} from "react-icons/cg"
+
+export const mainu=[
+    {title:"Home",icon:<AiOutlineHome className="text-2xl mr-5"></AiOutlineHome>, ativeIcon:<AiFillHome className="text-2xl mr-5"></AiFillHome>},
+    {
+        title:"Search", icon:<AiOutlineSearch className="text-2xl mr-5"></AiOutlineSearch>, activeIcon:<AiOutlineSearch className="text-2xl mr-5"></AiOutlineSearch>
+    },
+    {
+        title:"Explore", icon:<AiOutlineCompass className="text-2xl mr-5"></AiOutlineCompass>, activeIcon:<AiFillCompass className="text-2xl mr-5"></AiFillCompass>
+    },
+    {
+        title:"Reels", icon:<RiVideoLine className="text-2xl mr-5"></RiVideoLine>, activeIcon:<RiVideoFill className="text-2xl mr-5"></RiVideoFill>
+    },
+    {
+        title:"Message",
+        icon:<AiOutlineMessage className="text-2xl mr-5"></AiOutlineMessage>,
+        activeIcon:<AiFillMessage className="text-2xl mr-5"></AiFillMessage>
+    },
+    {
+        title:"Notification",
+        icon:<AiOutlineHeart className="text-2xl mr-5"></AiOutlineHeart>,
+        activeIcon:<AiOutlineHeart className="text-2xl mr-5"></AiOutlineHeart>
+    },
+    {
+        title:"Create",
+        icon:<AiOutlinePlusCircle className="text-2xl mr-5"></AiOutlinePlusCircle>,
+        activeIcon:<AiFillPlusCircle className="text-2xl mr-5"></AiFillPlusCircle>
+    },
+    {
+        title:"Profile",
+        icon:<CgProfile className="text-2xl mr-5"></CgProfile>,
+        activeIcon:<CgProfile className="text-2xl mr-5"></CgProfile>
+    },
+    {
+
+    }
+]
