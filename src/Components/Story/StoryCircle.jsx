76d45fc7169d04bcfd3eaaef82451ccae42167b1
gaeunSung <gannnni_10@naver.com>
2024-01-05 01:21:36 +0900

    story

---
+++ src/Components/Story/StoryCircle.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const StoryCircle = () => {
+  return (
+    <div className='cursor-pointer flex flex-col items-center'>
+      <img
+        className='w-16 h-16 rounded-full'
+        src='https://cdn.pixabay.com/photo/2023/09/04/10/31/swan-8232481_640.jpg'
+        alt=''
+      />
+      <p>username</p>
+    </div>
+  );
+};
+
+export default StoryCircle;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import StoryCircle from '../../Components/Story/StoryCircle';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div>
+        <div className='w-[44%] px-10'>
+          <div className='storyDiv flex space-x-2 border p-4 rounded-md justify-start w-full'>
+            {[1, 1, 11].map((item) => (
+              <StoryCircle />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
