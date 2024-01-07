5c935e0bce57ef03e68936467e2b317965dab7d9
gaeunSung <gannnni_10@naver.com>
2024-01-08 01:43:49 +0900

    homeRight

--- src/Components/HomeRight/HomeRight.jsx
+++ src/Components/HomeRight/HomeRight.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import SuggestionCard from './SuggestionCard';
 
 const HomeRight = () => {
-  return <div className='border'>HomeRight</div>;
+  return (
+    <div>
+      <div>
+        <div className='flex justify-between items-center'>
+          <div className='flex items-center'>
+            <div>
+              <img
+                className='w-12 h-12 rounded-full'
+                src='https://cdn.pixabay.com/photo/2023/03/11/20/12/moon-7845194_640.jpg'
+                alt=''
+              />
+            </div>
+            <div className='ml-3'>
+              <p>FullName</p>
+              <p className='opacity-70'>username</p>
+            </div>
+          </div>
+          <div>
+            <p className='text-blue-700 semibold'>switch</p>
+          </div>
+        </div>
+        <div className='space-y-5 mt-10'>
+          {[, 1, 1, 1, 11, 1].map((item) => (
+            <SuggestionCard />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeRight;
