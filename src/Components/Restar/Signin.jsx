51bbb937e7e7ec533c3f5752aabee9e3196bda71
gaeunSung <gannnni_10@naver.com>
2024-01-25 02:35:12 +0900

    sign in

--- src/Components/Restar/Signin.jsx
+++ src/Components/Restar/Signin.jsx
@@ -1,7 +1,114 @@
 import React from 'react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  Input,
+} from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invaild email address')
+    .required('email is Required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('password is required'),
+});
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const initialValues = { email: '', password: '' };
+  const navigate = useNavigate();
+
+  const handleSubmit = (values) => {
+    console.log('values: ', values);
+  };
+  return (
+    <div>
+      <div className='border'>
+        <Box
+          p={8}
+          display={'flex'}
+          flexDirection={'column'}
+          alignItems={'center'}
+        >
+          <img className='mb-5' src='https://i.imgur.com/zqpwkLQ.png' alt='' />
+
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+          >
+            {(formikProps) => (
+              <Form className='space-y-8'>
+                <Field name='email'>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.email && form.touched.email}
+                    >
+                      <Input
+                        className='w-full'
+                        {...field}
+                        id='email'
+                        placeholder='Mobile Number OR Email'
+                      ></Input>
+                      <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Field name='password'>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.password && form.touched.password}
+                    >
+                      <Input
+                        className='w-full'
+                        {...field}
+                        id='password'
+                        placeholder='password'
+                      ></Input>
+                      <FormErrorMessage>
+                        {form.errors.password}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <p className='text-center'>
+                  People who user our service may have contact information to
+                  Instagram. Learn More...
+                </p>
+
+                <p className='text-center'>
+                  By signing up, you agree to our Terms, Privacy Policy and
+                  Cookies Policy.
+                </p>
+
+                <Button
+                  className='w-full'
+                  mt={4}
+                  colorScheme='blue'
+                  type='submit'
+                  isLoading={formikProps.isSubmitting}
+                >
+                  Sign In
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </div>
+      <div className='border w-full border-slate-300 mt-5'>
+        <p className='text-center py-2 '>
+          If You Don't Have Account{' '}
+          <span className='ml-2 text-blue-700 cursor-pointer'>Sign Up</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -5,18 +5,18 @@ import Signin from '../../Components/Restar/Signin';
 const Auth = () => {
   return (
     <div>
-      <div className='flex items-center justify-center h-[100vh]'>
+      <div className='flex items-center justify-center h-[100vh] space-x-5'>
         <div className='relative hidden lg:block'>
-          <div className='h-[38.3rem] w-[27rem]'>
+          <div className='h-[38.5rem] w-[29rem]'>
             <img
               className='h-full w-full '
               src='https://velog.velcdn.com/images/seonghyun015b/post/5b308742-be7d-4b56-a693-2216746704e7/image.png'
               alt=''
             />
-            <div className='mobileWallpaper h-[33rem] w-[15.7rem] absolute top-6 right-2'></div>
+            <div className='mobileWallpaper h-[33rem] w-[15.7rem] absolute top-6 right-14 '></div>
           </div>
         </div>
-        <div>
+        <div className='w-[30vw]'>
           <Signin />
         </div>
       </div>
